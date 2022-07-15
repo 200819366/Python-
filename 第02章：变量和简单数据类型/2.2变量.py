998411b708139fe7cbc11832549c78f164283eec
獨飲品書香 <iblack@vip.qq.com>
2022-07-16 04:35:37 +0800

    22年7月16日学习

---
+++ 第02章：变量和简单数据类型/2.2变量.py
@@ -0,0 +1,5 @@
+message = "Hello Python world!"
+print(message)
+# 在程序中可随时修改变量的值，而 Python 将始终记录变量的最新值
+message = "Hello Python Crash Course world!"
+print(message)
---
+++ 第02章：变量和简单数据类型/2.3字符串.py
@@ -0,0 +1,26 @@
+# 使用方法修改字符串的大小写
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+# 储存数据时，方法lower()很好用。
+# 很多时候，你无法依靠用户来提供正确的大小写。
+# 因此需要将字符串先转换为小写，再储存他们。
+
+
+# 合并（拼接）字符串
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name+" " + last_name
+
+print(full_name)
+print("Hello," + " " + full_name.title()+" !")
+
+# 使用制表符或换行符来添加空白
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+### 删除空白
