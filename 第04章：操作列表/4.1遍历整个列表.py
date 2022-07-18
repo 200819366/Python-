791aeb583026e76380447caa2dd710b40ee2cfbe
獨飲品書香 <iblack@vip.qq.com>
2022-07-18 10:19:55 +0800

    2022年7月18日学习记录

---
+++ 第04章：操作列表/4.1遍历整个列表.py
@@ -0,0 +1,17 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(magician.title()+",that was a great trick!")
+
+print("---")
+
+for magician in magicians:
+    print(magician.title()+",that was a great trick!")
+    print("i can't wait to see you next trick,"+magician.title()+".\n")
+
+for magician in magicians:
+    print(magician.title()+",that was a great trick!")
+    print("i can't wait to see you next trick,"+magician.title()+".\n")
+print("Thank you,everyone.That was a great magic show!")
---
+++ 第04章：操作列表/4.3创建数值列表.py
@@ -0,0 +1,3 @@
+# 使用 range 函数
+for value in range(1, 5):
+    print(value)
